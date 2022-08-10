5f5e40f6e73525d3d8b65d66f74cb1135ca1a961
Lexus Banton <lexusbanton.kr@gmail.com>
2022-08-10 11:49:21 -0700

    kind done

--- app.js
+++ app.js
@@ -8,26 +8,99 @@ const optionBSubtractButton = document.getElementById('option-b-subtract-button'
 const finishPollButton = document.getElementById('finish-poll-button');
 const pastPollsContainer = document.getElementById('past-poll-container');
 
-
-
 // let state
-  let question = '';
-  let option1Votes = 0;
-  let option2Votes = 0;
-  let option1Title = '';
-  let option2Title = '';
-  let pastPolls = [];
+let question = '';
+let optionAVotes = 0;
+let optionBVotes = 0;
+let optionA = '';
+let optionB = '';
+let pastPolls = [];
 // set event listeners 
-newPollEl.addEventListener('submit', (e) =>  {
-  e.preventDefault();
-  const data = new FormData(newPollEl);
-  const userQuestion = data.get('question');
-  const userOptionA = data.get('option-A');
-  const userOptionB = data.get('option-B');
-  
-  question = userQuestion;
-  optionA = userOptionA;
-  optionB = userOptionB;
+newPollEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(newPollEl);
+    const userQuestion = data.get('question');
+    const userOptionA = data.get('option-A');
+    const userOptionB = data.get('option-B');
+    question = userQuestion;
+    optionA = userOptionA;
+    optionB = userOptionB;
+    
+    renderPoll();
+    displayCurrentPoll();
+});
+
+optionAAddButton.addEventListener('click', () => {
+    optionAVotes++;
+    displayCurrentPoll();
+});
+ 
+optionBAddButton.addEventListener('click', () => {
+    optionBVotes++;
+    displayCurrentPoll();
+});
+
+optionASubtractButton.addEventListener('click', () => {
+    optionAVotes--;
+    displayCurrentPoll();
+});
+
+optionBSubtractButton.addEventListener('click', () => {
+    optionBVotes--;
+    displayCurrentPoll();
+});
+
+finishPollButton.addEventListener('click', () => {
+    pastPollsContainer.textContent = '';
+    displayPastPolls();
+    optionAVotes = 0;
+    optionBVotes = 0;
+    currentPollEl.textContent = '';
+
+}
+);
+
+function renderPoll(question, optionA, optionB, optionAVotes, optionBVotes) {
+    const currentPollEl = document.createElement('div');
+    const questionEl = document.createElement('p');
+    const userOptionA = document.createElement('p');
+    const userOptionB = document.createElement('p');
+    const votesAEl = document.createElement('p');
+    const votesBEl = document.createElement('p');
+    questionEl.textContent = question;
+    userOptionA.textContent = optionA;
+    userOptionB.textContent = optionB;
+    votesAEl.textContent = optionAVotes;
+    votesBEl.textContent = optionBVotes;
+
+    currentPollEl.append(questionEl, userOptionA, userOptionB, optionAVotes, optionB);
+    return currentPollEl;
+
+}
+
+function displayCurrentPoll() {
+    newPollEl.reset();
+    currentPollEl.textContent = '';
+    const currentPollData = renderPoll(question, optionA, optionB, optionAVotes, optionBVotes);
+    currentPollEl.append(currentPollData);
+}
+
+function displayPastPolls() {
+    const pastPoll = {
+        question: question, 
+        optionA: optionA,
+        optionB: optionB,
+        optionAVotes: optionAVotes,
+        optionBVotes: optionBVotes
+
+    };
+    pastPolls.push(pastPoll);
+
+    for (let poll of pastPolls) {
+        const currentPastPoll = renderPoll(poll.question, poll.optionA, poll.optionB, poll.optionAVotes, poll.optionBVotes);
+        pastPollsContainer.append(currentPastPoll);
+    }
+
 }
   // get user input
   // use user input to update state 
